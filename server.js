ae34ef448d2a6f9c04c0ba802149d44105cd7b12
Pradhamlr <pradham.lr@gmail.com>
2025-02-07 03:33:42 +0530

    Third Commit

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-    useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
